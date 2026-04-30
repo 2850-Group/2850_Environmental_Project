4c87c90a313c3575a449db3ecf84c0f360c3515f
liamhallinan <liamrhallinan@gmail.com>
2026-05-01 00:04:27 +0100

    addded farmer/researcher roles on sign-up + profile shows user role now

--- Code/Database/Queries/sign_in.py
+++ Code/Database/Queries/sign_in.py
@@ -3,18 +3,16 @@ import bcrypt
 
 def sign_in_query(conn, cursor, user, pwd):
     pwd_byte = pwd.encode()
-    cursor.execute("SELECT password_hash FROM user WHERE username = ?", (user,))
+    cursor.execute("SELECT password_hash, role FROM user WHERE username = ?", (user,))
     row = cursor.fetchone()
     if row and bcrypt.checkpw(pwd_byte, row[0]):
-        return True
-    else:
-        return False
-    
-def sign_up_query(conn, cursor, name, user, pwd, fullname_val):
+        return row[1]
+    return None
+
+def sign_up_query(conn, cursor, name, user, pwd, fullname_val, role="Farmer"):
     pwd_byte = pwd.encode()
     salt = bcrypt.gensalt()
     pwd_hash = bcrypt.hashpw(pwd_byte, salt)
-    role = "User"
     cursor.execute("SELECT COUNT(user_id) FROM user WHERE username = ?", (user,))
     result = cursor.fetchone()
     if result[0] == 0:
