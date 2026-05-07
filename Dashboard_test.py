f966f91772878c3363b1c4192f78d20ec2430178
Isobel Martin <isobel@castlebarn.info>
2026-05-07 12:24:51 +0100

    SignUp tests passed.

--- Dashboard_test.py
+++ Dashboard_test.py
@@ -1,16 +1,23 @@
 """
 Testing: Dashboard
 
-We have chosen to use a mix of integrated and scripted testing for the dashbaord because of the UI demonstration.
+We have chosen to use a mix of integrated and scripted testing for the dashboard because of the UI demonstration.
 
 """
-from Dashboard import *
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import patch, MagicMock
+
+mock_model = MagicMock()
+patch('tensorflow.keras.models.load_model', return_value=mock_model).start()
+
+
+
 """
 SignUp Testing
 """
-class SignUpTest() :
+from Dashboard import SignUpPanel
+
+class SignUpTest(unittest.TestCase) :
 
     def make_screen(self, name="Jane Doe", username="janeD", password="12345678"):
         screen = MagicMock()
@@ -18,6 +25,9 @@ class SignUpTest() :
         screen.username.text = username
         screen.password.text = password
         screen._error = MagicMock()
+        screen.success_cb = MagicMock()
+
+        screen._on_sign_up = SignUpPanel._on_sign_up.__get__(screen)
         return screen
     
     #TEST 1: User leaves both username and password fields empty.
@@ -34,7 +44,7 @@ class SignUpTest() :
 
     #TEST 3: User leaves username field empty.
     def test_3(self):
-        screen = self.make_screen(name="")
+        screen = self.make_screen(username="")
         screen._on_sign_up()
         screen._error.show.assert_called_once_with("All fields are required")
     
@@ -44,9 +54,9 @@ class SignUpTest() :
         screen._on_sign_up()
         screen._error.show.assert_called_once_with("All fields are required")
 
-    #TEST 5: User uses a username already in the database.
+    #TEST 5: User uses a username already in the database. Prequisite there must be a user profile with the username already stored in the database.
     def test_5(self):
-        screen = self.make_screen(username="holly")
+        screen = self.make_screen(username="i.martin")
         screen._on_sign_up()
         screen._error.show.assert_called_once_with("Username is taken. Please choose another.")
 
@@ -59,24 +69,64 @@ class SignUpTest() :
     #TEST 7: User uses a unique username and a valid password.
     def test_7(self):
         screen = self.make_screen(username="BobM", name="Bob Muncan", password="12345678")
-        screen._on_sign_up()
-        #???
+        with patch('Dashboard.sign_up_query', return_value=True):
+            screen._on_sign_up()
+        screen.success_cb.assert_called_once()
+        screen._error.show.assert_not_called()
 
 
 """
 LogIn Testing
 """
-#TEST 1: User leaves both username and password fields empty.
+from Dashboard import SignInPanel 
+class SignInTest(unittest.TestCase) :
 
-#TEST 2: User leaves password field empty.
+    def make_screen(self, name="Jane Doe", username="janeD", password="12345678"):
+        screen = MagicMock()
+        screen.fullname.text = name
+        screen.username.text = username
+        screen.password.text = password
+        screen._error = MagicMock()
+        screen.success_cb = MagicMock()
 
-#TEST 3: User leaves username field empty.
+        screen._on_sign_in = SignInPanel._on_sign_in.__get__(screen)
+        return screen
+    
+    #TEST 1: User leaves both username and password fields empty.
+    def test_1(self):
+        screen = self.make_screen(username="", password="")
+        screen._on_sign_in()
+        screen._error.show.assert_called_once_with("Please enter your username and password")
 
-#TEST 4: User uses a username with an incorrect passowrd.
+    #TEST 2: User leaves password field empty.
+    def test_2(self):
+        screen = self.make_screen(password="")
+        screen._on_sign_in()
+        screen._error.show.assert_called_once_with("Please enter your username and password")
 
-#TEST 5: User uses a unique username and a valid password.
+    #TEST 3: User leaves username field empty.
+    def test_3(self):
+        screen = self.make_screen(username="")
+        screen._on_sign_in()
+        screen._error.show.assert_called_once_with("Please enter your username and password")
+    
+    #TEST 4: User uses a username with an incorrect passowrd.
+    def test_4(self):
+        screen = self.make_screen(username="izzy.martin")
+        screen = self.make_screen(password="1")
+        screen._on_sign_in()
+        screen._error.show.assert_called_once_with("Incorrect username or password.")
 
-#TEST 6: User attempts to log in 10 times.
+    #TEST 5: User uses a username and a valid password. Prerequsite, an account must be made with the details username="i.martin" and password="12345678"
+    def test_5(self):
+        screen = self.make_screen(username="i.martin")
+        screen = self.make_screen(password="12345678")
+        with patch('Dashboard.sign_in_query', return_value={"username": "i.martin"}):
+            screen._on_sign_in()
+        screen.success_cb.assert_called_once()
+        screen._error.show.assert_not_called()
+        
+
 
 """
 Dashboard Testing
